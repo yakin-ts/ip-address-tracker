3864290fd9fd8e237bc228d17629d0230dbfcdaf
Yakin Teshome <yakin16tm@gmail.com>
2024-01-10 23:21:11 +0300

    [feat]: setup styles and add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        'very-dark-grey': "hsl(0, 0%, 17%)",
+        'dark-grey': "hsl(0, 0%, 59%)",
+      },
+      fontFamily: {
+        'rubik': ['Rubik', 'sans-serif'],
+      },
+      backgroundImage: theme => ({
+        'desktop': "url('./images/pattern-bg-desktop.png')",
+        'mobile': "url('./images/pattern-bg-mobile.png')",
+      }),
+      plugins: [],
+    }
+
+  }
 }
