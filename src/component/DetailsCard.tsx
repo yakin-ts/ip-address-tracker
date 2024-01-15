e0b5e3e92b3e87ff2ba78431f100f402389a14d0
Yakin Teshome <yakin16tm@gmail.com>
2024-01-15 17:48:22 +0300

    [fix]: mobile view fix

--- src/component/DetailsCard.tsx
+++ src/component/DetailsCard.tsx
@@ -16,24 +16,24 @@ const DetailsCard: React.FC<DetailsCardProps> = ({ geoData }) => {
     }
 
     return (
-        <div className='absolute  top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5/6 p-5 flex flex-col md:justify-around md:py-8 items-center lg:grid lg:grid-cols-4 rounded-xl bg-white gap-4 shadow-xl font-rubik'>
-            <div className='flex flex-col justify-center items-center md:gap-4 lg:border-r-2 lg:border-gray-200 lg:not:last-child'>
-                <p className='text-very-dark-grey/50 text-sm font-semibold uppercase'>IP Address</p>
-                <p className='text-very-dark-grey text-xl font-bold'>
+        <div className='absolute  top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5/6 p-3 md:p-5 flex flex-col md:justify-around lg:py-8 items-center lg:grid lg:grid-cols-4 rounded-xl bg-white gap-3 lg:gap-4 shadow-xl font-rubik'>
+            <div className='flex flex-col justify-center items-center lg:gap-4 lg:border-r-2 lg:border-gray-200 lg:not:last-child'>
+                <p className='text-very-dark-grey/50 text-xs md:text-sm font-semibold uppercase'>IP Address</p>
+                <p className='text-very-dark-grey text-md md:text-lg lg:text-xl  font-bold text-center'>
                     {geoData.ip || <Loader />}
                 </p>
             </div>
             <div className='flex flex-col justify-center items-center md:gap-4 lg:border-r-2 lg:border-gray-200 lg:not:last-child'>
-                <p className='text-very-dark-grey/50 text-sm font-semibold uppercase'>Location</p>
-                <p className='text-very-dark-grey text-xl font-bold'>{geoData.location || <Loader />}</p>
+                <p className='text-very-dark-grey/50 text-xs md:text-sm font-semibold uppercase'>Location</p>
+                <p className='text-very-dark-grey text-md md:text-lg lg:text-xl  font-bold text-center'>{geoData.location || <Loader />}</p>
             </div>
             <div className='flex flex-col justify-center items-center md:gap-4 lg:border-r-2 lg:border-gray-200 lg:not:last-child'>
-                <p className='text-very-dark-grey/50 text-sm font-semibold uppercase'>Timezone</p>
-                <p className='text-very-dark-grey text-xl font-bold'>{geoData.time_zone ? `UTC ${timeZoneOffsetSign} ${Number(timeZoneOffsetHours)}:${timeZoneOffsetMinutes}` : <Loader />}</p>
+                <p className='text-very-dark-grey/50 text-xs md:text-sm font-semibold uppercase'>Timezone</p>
+                <p className='text-very-dark-grey text-md md:text-lg lg:text-xl  font-bold text-center'>{geoData.time_zone ? `UTC ${timeZoneOffsetSign} ${Number(timeZoneOffsetHours)}:${timeZoneOffsetMinutes}` : <Loader />}</p>
             </div>
             <div className='flex flex-col justify-center items-center md:gap-4'>
-                <p className='text-very-dark-grey/50 text-sm font-semibold uppercase'>ISP</p>
-                <p className='text-very-dark-grey text-xl font-semibold'>{geoData.isp || <Loader />}</p>
+                <p className='text-very-dark-grey/50 text-xs md:text-sm font-semibold uppercase'>ISP</p>
+                <p className='text-very-dark-grey text-md md:text-lg lg:text-xl font-semibold text-center'>{geoData.isp || <Loader />}</p>
             </div>
         </div>
     )
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC<HeaderProps> = ({ipAddress, onChange, onSubmit, loading, 
         <div className='w-full h-full flex flex-col bg-mobile lg:bg-desktop bg-cover font-rubik'>
             <h1 className='py-5 lg:py-8 text-white text-2xl font-semibold text-center'>IP Address Tracker</h1>
             {/* @ts-ignore */}
-            <form className='flex flex-row  h-12 mx-auto w-5/6 md:w-1/3 justify-center items-center  text-white' onSubmit={handleSubmit}>
+            <form className='flex flex-row  h-12 mx-auto w-5/6 md:w-2/3  lg:w-1/3 justify-center items-center  text-white' onSubmit={handleSubmit}>
                 <input className='w-3/4 h-full bg-white px-3 rounded-tl-xl rounded-bl-xl text-very-dark-grey focus:outline-none text-center lg:w-4/5' type='text' value={ipAddress} placeholder='Search for any IP address' onChange={onChange}/>
                 <button className='w-1/4 h-full bg-very-dark-grey flex justify-center items-center rounded-tr-xl rounded-br-xl lg:w-1/5' type='submit'>
                     {
